0657ef37bdd6ce321a99ba5e1154bd2c47b8a413
Try-Catch-Repeat <81193366+try-catch-repeat@users.noreply.github.com>
2021-03-28 07:52:23 +0300

    Fixes & additions

--- Machine Learning scripts/1_decision_trees_and_random_forest.py
+++ Machine Learning scripts/1_decision_trees_and_random_forest.py
@@ -13,8 +13,6 @@ def visualize_classifier_name(clf, output_name):
 	graph.render(f"1_decision_trees_and_random_forest/{output_name}") 
 
 
-
-
 def train_classifiers_on_dataset(X_train, X_test, y_train, y_test):
 	# Decision Tree classifier definitions
 	clf = tree.DecisionTreeClassifier(criterion='gini')
@@ -24,8 +22,6 @@ def train_classifiers_on_dataset(X_train, X_test, y_train, y_test):
 	print("Evaluation report for gini based decision tree")
 	print(classification_report(y_test, clf.predict(X_test)))
 
-
-
 	clf = tree.DecisionTreeClassifier(criterion='entropy')
 	clf = clf.fit(X_train, y_train)
 	visualize_classifier_name(clf, output_name='Decision Tree with information gain')
@@ -34,29 +30,22 @@ def train_classifiers_on_dataset(X_train, X_test, y_train, y_test):
 	print(classification_report(y_test, clf.predict(X_test)))
 
 	# Random forest with 100 estimators on gini criterion
-	clf = RandomForestClassifier(random_state=42,  n_estimators=100, criterion='gini')
-	clf.fit(X_train, y_train)
-
-	print("Evaluation report for gini based RF")
-	print(classification_report(y_test, clf.predict(X_test)))
+	# clf = RandomForestClassifier(random_state=42,  n_estimators=100, criterion='gini')
+	# clf.fit(X_train, y_train)
+	#
+	# print("Evaluation report for gini based RF")
+	# print(classification_report(y_test, clf.predict(X_test)))
 
 
 # Load dataset
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-print(X_train[:1], y_train[:1])
-
 train_classifiers_on_dataset(X_train, X_test, y_train, y_test)
-
 
 X, y = make_classification(n_samples=5000, n_features=25,
                            n_informative=12, n_redundant=0,
                            random_state=0, shuffle=False,
                            n_classes=10)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-print(X_train[:1], y_train[:1])
-
 
 train_classifiers_on_dataset(X_train, X_test, y_train, y_test)
